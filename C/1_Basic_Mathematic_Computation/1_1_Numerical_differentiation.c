eef6343a078fb5738005cd18e31bc834aa504df8
Yumeng Xu <yumengxu1994@icloud.com>
2016-03-06 21:53:05 +0800

    Add files .gitignore

---
+++ C/1_Basic_Mathematic_Computation/1_1_Numerical_differentiation.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+	int step;
+	int method;
+	scanf("Please choose the differentiation method",&method);
+	switch method
+
+	scanf("Please input the step of differentiation",&step);
+	return 0;
+}
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Computational-physics-example-code
 The practicing for Computational Physics, writing in Fortran, C, Matlab and Python.
+
+## Chapter 1: Basic Mathematic Computation
+
+* Numerical differentiation
